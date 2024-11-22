3597f87e2105fe0775c814e5ca770f3a47ceb55f
isabelpeedikayil <124645059+isabelpeedikayil@users.noreply.github.com>
2024-11-22 17:40:00 -0600

    Update Listener.java

--- Listener.java
+++ Listener.java
@@ -1,21 +1,23 @@
 import java.net.*;
-import java.util.HashSet;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Listener {
     public static void main(String[] args){
         try{
+            // input messages to get values
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter the port for the listener:");
+            System.out.println("Enter the port for the Listener:");
             int listener_port = scanner.nextInt();
 
-            // prep the socket 
+            // prep the socket to listen
             DatagramSocket socket = new DatagramSocket(listener_port);
 
+            // create a buffer that holds incoming data packets
             byte[] buffer = new byte[1024];
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
 
+            // receive the first message which has to be message 0
             socket.receive(packet);
             String message0 = new String(packet.getData()).trim();
             String[] parts = message0.split(":");
@@ -25,29 +27,27 @@ public class Listener {
             InetAddress talkerAddress = packet.getAddress();
             int talker_port = packet.getPort();
 
+            // send an ACK for initial message
             String ack0 = "ACK:0";
             socket.send(new DatagramPacket(ack0.getBytes(), ack0.length(), talkerAddress, talker_port));
 
             StringBuilder received_data = new StringBuilder();
             int expected_SeqNum = 1;
-            Random random = new Random();
+            Random random = new Random(); // simulates the random ack drops
 
-            //boolean all_messages_received_correctly = true;
-            //for (int i = 1; i <= total_messages; i++){
             while(expected_SeqNum <= total_messages){
-                //boolean received = false;
-                //while(!received){
                 socket.receive(packet);
+                // extracts data
                 String message = new String(packet.getData(), 0, packet.getLength());
                 System.out.println("Received: " + message);
 
+                // parse the message to extract the sequence number and data
                 parts = message.split(":");
                 int seqNum = Integer.parseInt(parts[0]);
                 String data = parts[1];
 
-                    //if(random.nextBoolean()){
-                HashSet<Integer> processedSeqNums = new HashSet<>();
                 if(seqNum == expected_SeqNum){
+                    // simulates random ack dropping
                     if(random.nextBoolean()){
                         String ack = "ACK:" + seqNum;
                         socket.send(new DatagramPacket(ack.getBytes(), ack.length(), talkerAddress, talker_port));
@@ -55,7 +55,6 @@ public class Listener {
                         
                         received_data.append(data);
                         expected_SeqNum++;
-                        processedSeqNums.add(seqNum);
                     }
                     else{
                         System.out.println("Dropped ACK for SeqNum: " + seqNum);
@@ -70,6 +69,7 @@ public class Listener {
             } else{
                 System.out.println("Failed to receive all messages.");
             }
+            // close the socket and throw exceptions
             socket.close();
             } catch (Exception e){
                 e.printStackTrace();
